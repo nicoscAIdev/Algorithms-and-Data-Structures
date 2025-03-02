3a470fc40dd3b194cb054e697d77539941eaa42c
nicoscAIdev <nicoscaidev@gmail.com>
2025-03-02 17:06:03 -0300

    A solved exercise is added

---
+++ 9- Subproblems and Functions/10- Portal de empleo.py
@@ -0,0 +1,81 @@
+#Un conocido portal de empleo requiere un programa para validar las búsquedas que se cargan en su página. Por cada
+#búsqueda se requiere:
+#CUIT, Descripción de la búsqueda, Salario ofrecido.
+
+#Si todos los datos son válidos, mostrar el aviso completo. En caso contrario, informar que no es posible mostrarlo.
+
+#Para terminar, consultar al usuario si desea cargar otro aviso o salir del programa
+
+def validar_cuit(cuit):
+    digito = 0
+    valido = False
+    if len(cuit) == 13:
+        if cuit[2] == "-" and cuit[11] == "-":
+             for car in cuit:
+                 if car in "0123456789":
+                    digito += 1
+             if digito == 11:
+                valido = True
+                return valido
+             else: print("El cuit ingresado no contiene 11 digitos.")
+        else: print("El CUIT ingresado no presenta los guiones '-' en las posiciones 3 y 12.")
+    else: print("El CUIT ingresado no contiene 13 caracteres.")
+
+def es_mayuscula(letra):
+    if letra >= 'A' and letra <= 'Z':
+       return True
+    else:
+         return False
+
+def validar_desc(desc):
+    anterior = " "
+    cant_palabras = 0
+    if len(desc) <= 60:
+        for letra in desc:
+            if letra == ' ' or letra == '.':
+                cant_palabras += 1
+
+            elif es_mayuscula(letra) and es_mayuscula(anterior):
+                print("El texto ingresado no debe contener dos mayusculas seguidas. ")
+                return False
+
+            anterior = letra
+
+        if cant_palabras >= 3:
+            return True
+        else:
+            print("La descripcion debe contener al menos 3 palabras. ")
+
+
+    else: return False
+
+
+def validar_salario(salario):
+
+    if salario > 0:
+        return True
+    else:
+        print("El salario debe ser mayor a 0! ")
+        return False
+
+
+def principal():
+    print("PORTAL DE EMPLEO")
+    # datos
+    cuit = input("Ingrese numero de CUIT: ")
+    desc = input("Ingrese descripcion: ")
+    salario = float(input("Ingrese salario: "))
+
+    #proceso
+    cuit_valido = validar_cuit(cuit)
+    desc_valido = validar_desc(desc)
+    salario_valido = validar_salario(salario)
+
+    # Salida
+    if cuit_valido and desc_valido and salario_valido:
+        print("El cuit: ", cuit)
+        print("Realiza la siguiente busqueda: ", desc)
+        print("Con un salario de: $", salario)
+
+    else: print("Consulta fallida. ")
+principal()
