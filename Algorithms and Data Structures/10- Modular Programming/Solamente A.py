8b724c997d35ae7e777b6f21470d2f723a980b97
nicoscAIdev <nicoscaidev@gmail.com>
2025-03-22 21:23:47 -0300

    half exercise done

--- Algorithms and Data Structures/10- Modular Programming/Solamente A.py
+++ Algorithms and Data Structures/10- Modular Programming/Solamente A.py
@@ -11,3 +11,37 @@ La longitud de la palabra más larga es 7 letras.
 Las palabras cuya única vocal es la 'a' son: 3.
 El porcentaje de estas palabras sobre el total es 43%.
 """
+
+def enterText():
+    words = input("Ingresar palabras por teclado, separadas por un espacio y terminado en punto: ")
+    return words
+
+def processText(words):
+    contCar = 0
+    formWord = mostLarge = ""
+    for car in words:
+        contCar += 1
+        
+        if contCar == 1 and car == " ":
+            contCar = 0
+            break
+       
+        formWord += car
+       
+        if car == " " or car == ".":
+            if len(mostLarge) < len(formWord):
+                mostLarge = formWord
+                
+            # Reiniciamos variables
+            formWord = ""
+            contCar = 0
+        
+    return mostLarge
+    
+def test():    
+    words = enterText()
+    print("\nResultados:")
+    print(words)
+    print(processText(words))
+
+test()
