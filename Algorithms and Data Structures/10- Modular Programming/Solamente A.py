a4652d364e7aabc1a80143be88427ed3eac6e5fb
nicoscAIdev <nicoscaidev@gmail.com>
2025-03-23 15:44:53 -0300

    Problem solved

--- Algorithms and Data Structures/10- Modular Programming/Solamente A.py
+++ Algorithms and Data Structures/10- Modular Programming/Solamente A.py
@@ -16,32 +16,63 @@ def enterText():
     words = input("Ingresar palabras por teclado, separadas por un espacio y terminado en punto: ")
     return words
 
+def calcPorc(contWord, wordWhithA):
+    return (wordWhithA*100/contWord)
+
+
 def processText(words):
-    contCar = 0
+    contCar = wordWhithA = 0
     formWord = mostLarge = ""
+    contWord = 0
+    contA = 0
+
     for car in words:
-        contCar += 1
         
-        if contCar == 1 and car == " ":
+        if contCar == 1 and car == " " :
             contCar = 0
-            break
+            continue
        
         formWord += car
-       
+
+        # Identificamos una palabra     
         if car == " " or car == ".":
+        #   La conotamos
+            contWord += 1
+
+        #   Evaluamos si es la palabra mas larga
             if len(mostLarge) < len(formWord):
                 mostLarge = formWord
+
+            # Verificamos si la palabra solo tiene la vocal 'a'
+            tiene_a = False
+            tiene_otra_vocal = False
+
+            for letra in formWord:
+                if letra in "aeiou":  # Si es una vocal
+                    if letra == "a":
+                        tiene_a = True
+                    else:
+                        tiene_otra_vocal = True  # Tiene otra vocal
                 
+            if tiene_a and not tiene_otra_vocal:  # Solo tiene 'a'
+                wordWhithA += 1
+
             # Reiniciamos variables
             formWord = ""
             contCar = 0
-        
-    return mostLarge
+
+
+    return mostLarge, contWord, wordWhithA
+
+
     
+
 def test():    
     words = enterText()
+    mostLarge, contWord, wordWhithA = processText(words)
     print("\nResultados:")
     print(words)
-    print(processText(words))
-
+    print(mostLarge)
+    print(wordWhithA)
+    print(calcPorc(contWord, wordWhithA))
 test()
