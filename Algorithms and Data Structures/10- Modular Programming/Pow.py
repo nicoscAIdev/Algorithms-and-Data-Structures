32aa669a2fa6f79c457133181263085f86a46a1a
nicoscAIdev <nicoscaidev@gmail.com>
2025-04-02 12:18:30 -0300

    Point two added

--- Algorithms and Data Structures/10- Modular Programming/Pow.py
+++ Algorithms and Data Structures/10- Modular Programming/Pow.py
@@ -19,3 +19,61 @@ El programa debe:
 
 4 Determinar la cantidad de palabras que contienen "men" en la primera mitad de la palabra.
 """
+
+
+def contar_palabras_con_vocales(text):
+    cont_palabras_vocal = cont_vocal = cant_palabra = 0
+
+    for car in text:
+        if car in "aeiouAEIOU":
+            cont_vocal += 1
+        elif car == " " or car == ".":  # Fin de palabra
+            cant_palabra += 1
+
+            if cont_vocal == 3:
+                cont_palabras_vocal += 1
+            
+            cont_vocal = 0  # Reiniciar contador para la siguiente palabra
+
+    return cont_palabras_vocal, cant_palabra
+
+
+
+def proces_text(text):
+    cont_letras = digito_y_cuatro = 0
+    num_exist = False
+
+    for car in text:
+
+        if car in "123456789":
+            num_exist = True
+       
+        if car.isalpha():
+            cont_letras += 1
+
+        elif car == " " or car == ".":  # Fin de palabra
+            if num_exist == True and cont_letras >= 4:
+                digito_y_cuatro += 1
+
+            cont_letras = 0  # Reiniciar contador para la siguiente palabra
+            num_exist = False
+
+    return digito_y_cuatro
+
+
+def porcentaje(cantidad, base):
+    return (cantidad * 100) / base
+
+
+def principal():
+    text = input("Ingrese texto: ")
+
+    cont_palabras_vocal, cant_palabra = contar_palabras_con_vocales(text)
+    digito_y_cuatro = proces_text(text)
+
+    porc = porcentaje(digito_y_cuatro, cant_palabra)
+
+    print(cont_palabras_vocal)
+    print(f"{porc:.2f}%")
+
+principal()
