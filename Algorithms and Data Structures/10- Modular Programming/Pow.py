4d60a88cbb984de2ee7e0a8cf36a0c2b1eea6057
nicoscAIdev <nicoscaidev@gmail.com>
2025-04-01 22:16:24 -0300

    Exercise added

---
+++ Algorithms and Data Structures/10- Modular Programming/Pow.py
@@ -0,0 +1,21 @@
+"""
+Desarrollar un programa en Python que permita cargar por teclado un texto completo. 
+Se supone que el usuario cargará un punto para indicar el final del texto, 
+y que cada palabra de ese texto está separada de las demás por un espacio en blanco. 
+
+El programa debe:
+
+1 Determinar la cantidad de palabras que tuvieron exactamente 3 vocales.
+
+2 Determinar el porcentaje de palabras que tuvieron algún dígito ('0' al '9') y más de 4 letras.
+
+3 De las palabras que terminan con la primera letra de todo el texto, 
+    determinar el orden de la que tiene menor cantidad de caracteres.
+    Ejemplo: en el texto "Ana está en la casa":
+    - La primera letra del texto es 'A'.
+    - Las palabras que terminan con 'A' son: 'Ana', 'está', 'la', 'casa'.
+    - La palabra más corta es "la", que es la cuarta palabra del texto.
+    - El orden de la palabra más corta es 4.
+
+4 Determinar la cantidad de palabras que contienen "men" en la primera mitad de la palabra.
+"""
