9e515dca684fb4fe867934e22f38ed4aa3036ec5
nicoscAIdev <nicoscaidev@gmail.com>
2025-07-09 23:07:27 -0300

    Exercise added

---
+++ Algorithms and Data Structures/Parcial 4/Segundo simulacro/Ejercicio Tipo Parcial 4 - AED.py
@@ -0,0 +1,34 @@
+# Ejercicio Tipo Parcial 4 - AED
+
+# Un colegio secundario de la ciudad está organizando las actividades recreativas de la vuelta a la presencialidad.
+# Tiene pensado organizar una semana de actividades deportivas y requiere un software para gestionar las inscripciones
+# de alumnos en los diferentes deportes.
+
+# Para ello requieren cargar los datos de los alumnos. De cada alumno se conoce:
+# - el legajo (un número entero),
+# - el nombre,
+# - el año que cursa (un número entre 1 y 7),
+# - y un código de 0 a 9 que indica el deporte al que se inscribe.
+#     (0 = básquet, 1 = rugby seven, 2 = tenis, etc.)
+
+# Conociendo esta información, se pide crear un menú de opciones:
+
+# Parcial 1:
+
+# 1. Cargar los datos de los estudiantes en un vector de registros o generarlos de forma aleatoria.
+#    Se puede elegir una de las dos opciones, pero no mezclarlas.
+#    Los datos deben agregarse al vector de forma tal que queden ordenados alfabéticamente por nombre a medida que se cargan.
+#    * La solución de cargar todo y ordenar al final no será tenida en cuenta.
+
+# 2. Buscar en el vector si existe un alumno con nombre x_nombre (x_nombre se carga por teclado).
+#    Si existe, mostrar sus datos; si no existe, informar con un mensaje.
+
+# 3. Generar una matriz donde se cuente la cantidad de alumnos por deporte y año que cursa.
+#    (10 deportes * 7 años = 70 contadores).
+#    Mostrar los contadores por deporte y año, incluyendo aquellos que no hayan tenido inscriptos.
+
+# 4. Generar un archivo con los alumnos de un año x_anio (x_anio se carga por teclado).
+#    Si el archivo existe, reemplazar su contenido.
+
+# 5. Mostrar el contenido del archivo creado en el punto 5.
+
